ab925282ccb99bf1aec19e43465157a638684896
colorlessenergy <brianmunozdev@gmail.com>
2021-11-20 07:07:58 -0800

    when the form is finished save the tasks into localStorage and redirect to app page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 import Nav from '../shared/components/Nav';
 import First from '../shared/components/Forms/First';
 import Second from '../shared/components/Forms/Second';
 import Third from '../shared/components/Forms/Third';
+import Fourth from '../shared/components/Forms/Fourth';
 
 export default function Home () {
     const [ tasks, setTasks ] = useState({
@@ -15,6 +17,14 @@ export default function Home () {
     });
 
     const [ formPart, setFormPart ] = useState(0);
+
+    const router = useRouter(); 
+    useEffect(() => {
+        if (formPart === 'done') {
+            localStorage.setItem('tasks', JSON.stringify(tasks));
+            router.replace('/app')
+        }
+    }, [ formPart ])
     return (
         <div>
             <Head>
@@ -41,6 +51,13 @@ export default function Home () {
 
                 { formPart === 2 ? (
                     <Third
+                        tasks={ tasks.allTasks }
+                        setTasks={ setTasks }
+                        setFormPart={ setFormPart } />
+                ) : (null) }
+
+                { formPart === 3 ? (
+                    <Fourth
                         tasks={ tasks.allTasks }
                         setTasks={ setTasks }
                         setFormPart={ setFormPart } />
