cb8b1620305414491be24620a5e166ba248ff9de
colorlessenergy <brianmunozdev@gmail.com>
2022-11-09 08:47:54 -0800

    modify edit icon svg

--- shared/components/Task.js
+++ shared/components/Task.js
@@ -65,7 +65,6 @@ const Task = ({ task, taskType, setTasks, openEditTaskModal }) => {
                             height="24"
                             className="icon"
                         >
-                            <path fill="none" d="M0 0h24v24H0z" />
                             <path d="M12.9 6.858l4.242 4.243L7.242 21H3v-4.243l9.9-9.9zm1.414-1.414l2.121-2.122a1 1 0 0 1 1.414 0l2.829 2.829a1 1 0 0 1 0 1.414l-2.122 2.121-4.242-4.242z" />
                         </svg>
                     </button>
