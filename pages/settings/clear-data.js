9e301ab6eff63f679df3719c501be3f117f8f0b2
colorlessenergy <brianmunozdev@gmail.com>
2022-11-21 13:02:30 -0800

    add functionality to clear tasks, streak or taskCount

--- pages/settings/clear-data.js
+++ pages/settings/clear-data.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { useState } from 'react';
 
 import Nav from '../../shared/components/Nav';
 import Toast from '../../shared/components/Toast';
@@ -6,15 +7,36 @@ import useToast from '../../shared/hooks/useToast';
 
 const ClearData = () => {
     const clearData = () => {
-        localStorage.setItem('ID', JSON.stringify(0));
-        localStorage.setItem('tasks', JSON.stringify({}));
-        localStorage.setItem('streak', JSON.stringify({}));
-        localStorage.setItem('taskCount', JSON.stringify({}));
+        if (form.tasks) {
+            localStorage.setItem('ID', JSON.stringify(0));
+            localStorage.setItem('tasks', JSON.stringify({}));
+        }
+
+        if (form.streak) {
+            localStorage.setItem('streak', JSON.stringify({}));
+        }
+
+        if (form.taskCount) {
+            localStorage.setItem('taskCount', JSON.stringify({}));
+        }
 
         addToast('data cleared');
     };
 
     const { toast, addToast } = useToast();
+
+    const [form, setForm] = useState({
+        tasks: true,
+        streak: true,
+        taskCount: true
+    });
+
+    const handleOnChange = event => {
+        setForm(previousForm => ({
+            ...previousForm,
+            [event.target.id]: !previousForm[event.target.id]
+        }));
+    };
 
     return (
         <div>
@@ -29,10 +51,59 @@ const ClearData = () => {
 
                 <h1 className="m-0 mb-1 font-size-4">clear data</h1>
 
-                <p className="mb-2 font-size-1">clear tasks and stats</p>
+                <p className="mb-1 font-size-1">clear tasks and stats</p>
+
+                <form className="mb-1">
+                    <input
+                        type="checkbox"
+                        onChange={handleOnChange}
+                        className="d-none"
+                        id="tasks"
+                        checked={form.tasks}
+                    />
+                    <label
+                        className="flex align-items-center p-1 pl-0 cursor-pointer font-size-2"
+                        htmlFor="tasks"
+                    >
+                        <span className="checkbox mr-1"></span>
+                        tasks
+                    </label>
+
+                    <input
+                        type="checkbox"
+                        onChange={handleOnChange}
+                        className="d-none"
+                        id="streak"
+                        checked={form.streak}
+                    />
+                    <label
+                        className="flex align-items-center p-1 pl-0 cursor-pointer font-size-2"
+                        htmlFor="streak"
+                    >
+                        <span className="checkbox mr-1"></span>
+                        streak
+                    </label>
+
+                    <input
+                        type="checkbox"
+                        onChange={handleOnChange}
+                        className="d-none"
+                        id="taskCount"
+                        checked={form.taskCount}
+                    />
+
+                    <label
+                        className="flex align-items-center p-1 pl-0 cursor-pointer font-size-2"
+                        htmlFor="taskCount"
+                    >
+                        <span className="checkbox mr-1"></span>
+                        taskCount
+                    </label>
+                </form>
 
                 <button
                     onClick={clearData}
+                    disabled={!form.tasks && !form.streak && !form.taskCount}
                     title="clear data"
                     className="button background-color-dark-red color-white"
                 >
